290ead5890e5ae4bac070909aa16ff99165ec213
Cy4Nid8Hack <cy4nid8@protonmail.com>
2023-08-28 17:44:09 +0700

    create server and run

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 import express from 'express'
-import bcryptjs from 'bcryptjs'
+// import bcryptjs from 'bcryptjs'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
+const port = process.env.PORT||5000
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.get('/', (req, res) => res.send('Server is Running'))
+
+app.listen(port,()=>{console.log(`Server Started on PORT ${port}`)})
